2bb41b42468d6457066adccf2c32115f8ca26d9e
ThaisCampanac <thais.campanac@gmail.com>
2022-03-14 12:03:04 -0500

    Update Under Construction

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,16 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
     return (
-        <div className={styles.container}>
-            <h1>Under Construction</h1>
+        <div className='main-wrapper'>
+            <div className="under-construction">
+                <div className='logo'>
+                    <h3>C I C S</h3>
+                    <h4>General Contractor</h4>
+                </div>
+                <h1>Under Construction</h1>
+                <h2>Site Nearly Ready</h2>
+            </div>
         </div>
+
     )
 }
