aad5c70fbc1753b985f39ba0b45cec5685ba30d3
ThaisCampanac <thais.campanac@gmail.com>
2022-05-28 19:02:53 -0500

    add css for About Us

--- pages/index.js
+++ pages/index.js
@@ -25,8 +25,8 @@ export default function Home() {
                 <h3 className='subtitle'>
                     About Us
                 </h3>
-                <p>This is a place holder.</p>
-                <p>This is a place holder.</p>
+                <p className='info'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className='info'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
             </div>
             <div id='what-we-do' className='What-we-do'>
                 <h3 className='subtitle'>
